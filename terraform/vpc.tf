997b3d13cfd10f43cfff1e0e8b2724594dee97bb
y <y@gmail.com>
2026-03-22 10:00:09 +0530

    final

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -4,7 +4,7 @@ module "vpc" {
 
   name = "vprofile-eks"
 
-  cidr = "172.20.0.0/1"
+  cidr = "172.20.0.0/16"
   azs  = slice(data.aws_availability_zones.available.names, 0, 3)
 
   private_subnets = ["172.20.1.0/24", "172.20.2.0/24", "172.20.3.0/24"]
